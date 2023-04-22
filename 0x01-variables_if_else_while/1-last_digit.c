72afff73cfa2afdba384d99d2db2eee138dbbee3
root <root@DESKTOP-VI8S758>
2023-04-22 19:40:34 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,9 +13,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	ld = n % 10;
-	if (ld > 5)
-	{	
-		printf("last digit of %d id %d and is greater than 5\n", n, ld);
+
         }	
 	else if (ld == 0)
 	{
