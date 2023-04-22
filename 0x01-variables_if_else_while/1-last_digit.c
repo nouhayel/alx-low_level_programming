da574ce2a5c2a162a7c183e8494cda76afd5a7d3
root <root@DESKTOP-VI8S758>
2023-04-22 16:28:58 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@ int main(void)
          int n;
          int ld;
 
-         stand(time(0));
+         srand(time(0));
          n = rand() - RAND_MAX / 2;
          ld = n % 10;
          if (ld > 5)
