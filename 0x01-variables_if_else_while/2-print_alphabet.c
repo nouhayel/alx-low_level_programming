d8631e52eea3b404c13d28eb6944dfee2364ceff
root <root@DESKTOP-VI8S758>
2023-04-22 22:50:49 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print alphabets  lowercase'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar(65);
+	putchar('\n');
+	return (0);
+}	
