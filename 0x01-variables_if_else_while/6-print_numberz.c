38172cd6d878b4cd2d0ce97a0c5fbeae3cb8f609
root <root@DESKTOP-VI8S758>
2023-04-23 23:31:06 +0000

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,14 +4,15 @@
  * main - prints single sigit in base 10
  * Return: Always 0
  */
-int lain(void)
+int main(void)
 {
 	int num;
 
 	for (num = 0; num < 10; num++)
-	        putchar((num % 10) + '0');
+		putchar((num % 10) + '0');
+	
+	 putchar('\n');
 
-	putchar('\n');
+	 return (0);
 
-        return (0);
 }
